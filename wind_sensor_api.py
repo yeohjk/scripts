a59d8b66d9c592bb2ab27f87ac4636dff5730b60
YeohJK <spin8053@gmail.com>
2025-01-03 14:47:28 +0800

    Added import time statement. Added while loop for query and response method.

--- wind_sensor_api.py
+++ wind_sensor_api.py
@@ -1,8 +1,9 @@
-#Import statement for pyserial
+#Import statement for pyserial and time
 import serial
+import time
 
 #Class for wind sensor ICD query and response
-class icd_wind_speed:
+class wind_speed_sensor:
     def __init__(self):
         self.query_hex_string = '010300000001840A'
         self.query_bytes = bytes.fromhex(self.query_hex_string)
@@ -10,17 +11,20 @@ class icd_wind_speed:
         print(type(self.query_bytes))
         print(len(self.query_bytes))
         return
-    def query_response(self):
-        with serial.Serial('COM8', timeout=1) as ser:
-             ser.write(self.query_bytes)
-             self.response_bytes = ser.read(7)
-             print(self.response_bytes)
-             self.wind_speed_bytes = self.response_bytes[3:5]
-             print(self.wind_speed_bytes.hex())
-             self.wind_speed = int(self.wind_speed_bytes.hex(), 16)/100
-             print("Wind Speed = " + str(self.wind_speed) + " m/s")
+    #Query and Response method with time interval between measurements
+    def query_response(self, interval):
+        while True:
+            with serial.Serial('COM8', timeout=1) as ser:
+                ser.write(self.query_bytes)
+                self.response_bytes = ser.read(7)
+                print(self.response_bytes)
+                self.wind_speed_bytes = self.response_bytes[3:5]
+                print(self.wind_speed_bytes.hex())
+                self.wind_speed = int(self.wind_speed_bytes.hex(), 16)/100
+                print("Wind Speed = " + str(self.wind_speed) + " m/s")
+            time.sleep(int(input("Interval Time(s): ")))
         return
 
-
-query_1 = icd_wind_speed()
+#Wind speed sensor query and response execution
+query_1 = wind_speed_sensor()
 query_1.query_response()
